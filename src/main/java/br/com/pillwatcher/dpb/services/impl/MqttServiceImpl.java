774b2e4f981a6c80979c0a3d38b1ab9172ebdf3a
Filipe Dias <lipao.dias@hotmail.com>
2020-11-05 20:14:08 -0300

    :RECYcle: Add IOT config
    Co-authored-by: pAmanda <amandoca_779@hotmail.com>
    Co-authored-by: KahCosta <kamillacosta.unb@gmail.com>
    Co-authored-by: gabrielademoraes <gabrielademoraes10@gmail.com>
    Co-authored-by: geovanne97 <geovannessaraiva97@gmail.com>

--- src/main/java/br/com/pillwatcher/dpb/services/impl/MqttServiceImpl.java
+++ src/main/java/br/com/pillwatcher/dpb/services/impl/MqttServiceImpl.java
@@ -1,7 +1,10 @@
 package br.com.pillwatcher.dpb.services.impl;
 
+import br.com.pillwatcher.dpb.config.Mqtt;
 import br.com.pillwatcher.dpb.entities.MqttPublish;
+import br.com.pillwatcher.dpb.exceptions.BaseException;
 import br.com.pillwatcher.dpb.services.MqttService;
+import com.google.gson.Gson;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.paho.client.mqttv3.MqttException;
@@ -14,7 +17,7 @@ import org.springframework.stereotype.Service;
 public class MqttServiceImpl implements MqttService {
 
     @Override
-    public MqttMessage createMqttMessage(final MqttPublish publishModel) throws MqttException {
+    public MqttMessage createMqttMessage(final MqttPublish publishModel) {
 
         log.info("MqttServiceImpl.publishMessage() - Start");
         log.debug("MqttServiceImpl.publishMessage() - Start - {}", publishModel);
@@ -25,5 +28,24 @@ public class MqttServiceImpl implements MqttService {
         mqttMessage.setRetained(publishModel.getRetained());
 
         return mqttMessage;
+    }
+
+    @Override
+    public void setupMqtt(final Object object, final String topic, final BaseException defaultException)  {
+
+        MqttPublish mqttPublish = MqttPublish.builder()
+                .message(new Gson().toJson(object))
+                .qos(0)
+                .topic(topic)
+                .retained(true)
+                .build();
+
+        MqttMessage mqttMessage = createMqttMessage(mqttPublish);
+
+        try {
+            Mqtt.getInstance().publish(mqttPublish.getTopic(), mqttMessage);
+        } catch (MqttException e) {
+            System.out.println(e); throw defaultException;
+        }
     }
 }
